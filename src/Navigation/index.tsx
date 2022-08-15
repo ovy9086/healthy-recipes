e23befa46bf5f3c9d1bbac4f980fe25c52f92941
Ovidiu Latcu <ovidiu.latcu@gmail.com>
2022-08-15 13:11:05 +0300

    feat(): add navigation

---
+++ src/Navigation/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../Screens/Home';
+import DetailsScreen from '../Screens/Details';
+
+const Stack = createNativeStackNavigator();
+
+function NavigationRouter() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default NavigationRouter;
